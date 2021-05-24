c021d166e5e153dbdb4e47e57b2606a414fce051
aprihodko22 <tohathebest97@gmail.com>
2021-05-24 22:09:54 +0300

    add priv22 dva raza

--- src/main/java/Pres.java
+++ src/main/java/Pres.java
@@ -2,5 +2,6 @@ public class Pres {
     public static void main(String [] args){
         System.out.println("Priv");
         System.out.println("Priv22");
+        System.out.println("Priv22");
     }
 }
